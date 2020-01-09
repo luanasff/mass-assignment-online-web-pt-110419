03302a2831d1d33fc364784c7843de2f0c39c78c
Luana Faria Lane <luanasfff@gmail.com>
2020-01-09 05:29:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,5 +2,9 @@ class Person
   attr_accessor :name , :birthday , :hair_color , :eye_color, :height , :weight , :handed , :complexion , :t_shirt_size , :wrist_size , :glove_size , :pant_length , :pant_length , :pant_width 
   
   def initialize ()
-    @name = name: 
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @
 end
